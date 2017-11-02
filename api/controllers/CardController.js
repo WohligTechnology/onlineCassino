0c19fc013de3b60d1ddef2a60372cfabbb050297
Chintan <chintan@wohlig.com>
2017-11-02 10:10:25 +0530

    ok

--- api/controllers/CardController.js
+++ api/controllers/CardController.js
@@ -1,7 +1,6 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-    createCards : function(req, res){
-        console.log('reached');
+    createCards: function (req, res) {
         Card.createCards(res.callback);
     }
 };
