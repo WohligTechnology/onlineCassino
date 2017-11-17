3a2e70db9f6265d3704f215b259bf6ab1db2788f
Chintan <chintan@wohlig.com>
2017-11-17 13:14:05 +0530

    changes for the backup

--- tasks/config/mongobackup.js
+++ tasks/config/mongobackup.js
@@ -1,14 +1,21 @@
 module.exports = function (grunt) {
 
     grunt.config.set('mongobackup', {
-        import: {
+        dump: {
             options: {
+                host: 'localhost',
                 db: 'poker',
+                out: './dump'
+            }
+        },
+        restore: {
+            options: {
+                db: 'poker2',
                 host: 'localhost',
                 drop: true,
-                path: './import'
+                path: './dump/poker'
             }
-        }
+        },
     });
 
     grunt.loadNpmTasks('mongobackup');
