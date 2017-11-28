93753480c362390cad65b6b5f5a0b3799de888e9
Chintan <chintan@wohlig.com>
2017-11-28 17:38:19 +0530

    changes for the settings

--- api/services/Setting.js
+++ api/services/Setting.js
@@ -28,6 +28,44 @@ var model = {
                 }
             });
         });
+    },
+    search: function (data, callback) {
+        data = {};
+        var Model = this;
+        var Const = this(data);
+        var maxRow = Config.maxRow;
+
+        var page = 1;
+        if (data.page) {
+            page = data.page;
+        }
+        var field = data.field;
+
+
+
+
+        var options = {
+            field: data.field,
+            filters: {
+                keyword: {
+                    fields: ['name'],
+                    term: data.keyword
+                }
+            },
+            sort: {
+                asc: 'name'
+            },
+            start: (page - 1) * maxRow,
+            count: maxRow
+        };
+
+
+        var Search = Model.find(data.filter)
+
+            .order(options)
+            .keyword(options)
+            .page(options, callback);
+
     }
 };
 module.exports = _.assign(module.exports, exports, model);
