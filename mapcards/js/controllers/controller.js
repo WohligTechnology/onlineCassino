6ff3fae1c8b738d57d620a1273f09e764d205be2
Chintan <chintan@wohlig.com>
2017-12-03 11:50:53 +0530

    changes

--- mapcards/js/controllers/controller.js
+++ mapcards/js/controllers/controller.js
@@ -1,4 +1,11 @@
 var updateSocketFunction = {};
 myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationService, $timeout, apiService, $uibModal) {
-
+    $scope.template = TemplateService.getHTML("content/home.html");
+    TemplateService.title = "Home"; //This is the Title of the Website
+    TemplateService.header = ""; //This is the Title of the Website
+    TemplateService.footer = ""; //This is the Title of the Website
+    $scope.allCards = TemplateService.getAllCard();
+    $scope.mapCard = {
+        selected: _.head($scope.allCards)
+    };
 });
