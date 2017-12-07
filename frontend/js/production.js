66326d409fb906d66fc6555b6307979c63329558
Chintan <chintan@wohlig.com>
2017-12-07 19:28:59 +0530

    done

--- frontend/js/production.js
+++ frontend/js/production.js
@@ -66869,7 +66869,6 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
         if (data.undo) {
             $scope.removeWinner();
         }
-        console.log(data);
         if (data.extra && data.extra.player) {
             var x = document.getElementById("cardAudio");
             x.play();
